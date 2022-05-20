a594b397c10089e27ab7dc1d0eddc4bd9d23d7aa
Daroz <geraldodarosfilho@hotmail.com>
2022-05-19 22:21:50 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
