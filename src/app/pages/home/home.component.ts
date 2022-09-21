d889c5cbddec01f6952420ab5d91779ff28bb338
geraldodf <geraldodarosfilho@hotmail.com>
2022-09-21 18:12:45 -0300

    Começando testes para renderizar lista de produto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,30 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {Produto} from "../../../models/Produto";
-import {ProdutoServiceService} from "../produto-service.service";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private produtoService: ProdutoServiceService) { }
-
-  listaDeProdutos: Produto[] = [];
-
-  pegarTodosProdutos(){
-    this.produtoService.pegarProdutos().subscribe( resposta => {
-      this.listaDeProdutos = resposta;
-      alert(resposta)
-    }, error => {
-      console.log("Deu erro aqui véi")
-    })
-  }
-
-  ngOnInit(): void {
-    this.pegarTodosProdutos()
-    console.log("Aqui ------------------------------")
-  }
 
 }
--- src/app/templates/produtos/produtos.component.ts
+++ src/app/templates/produtos/produtos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProdutoServiceService} from "../../pages/produto-service.service";
+import {Produto} from "../../../models/Produto";
 
 @Component({
   selector: 'app-produtos',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private produtoService: ProdutoServiceService) {
   }
 
+  listaDeProdutos: Produto[] = [];
+
+  pegarTodosProdutos() {
+    this.produtoService.pegarProdutos().subscribe(resposta => {
+      this.listaDeProdutos = resposta;
+      alert(resposta.forEach(p => p.quantidade))
+      console.log(this.listaDeProdutos)
+      console.log(this.listaDeProdutos.forEach(p => p))
+    }, error => {
+      console.log("Deu erro em pegar todos os produtos!")
+    })
+  }
+
+  ngOnInit(): void {
+    this.pegarTodosProdutos()
+    console.log(this.listaDeProdutos.forEach( p => p))
+  }
 }
