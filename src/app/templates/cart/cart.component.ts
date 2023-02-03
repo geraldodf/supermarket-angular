f671b32e066ac4250b961bcabe08f68813fcbbda
Geraldo Daros <78937716+geraldodf@users.noreply.github.com>
2023-02-03 22:23:20 +0100

    Solving bugs and finalizing business rules in the cart and also its functionalities.

--- src/app/templates/cart/cart.component.ts
+++ src/app/templates/cart/cart.component.ts
@@ -13,30 +13,58 @@ export class CartComponent implements OnInit {
   }
 
   productList: Product[] = [];
+  salePrice: number = 0;
 
   ngOnInit() {
     this.loadProducts();
     this.initCart();
+    this.updateProductSalePrice();
+    this.updateSalePrice()
   }
 
-  initCart(){
-    this.productService.getProductsCart().forEach(p => this.addProduct(p))
+  initCart() {
+    this.productService.getProductsCart().forEach(p => {
+      this.addProduct(p)
+    })
   }
-  addProduct(product: Product) {
-    this.productList.push(product);
+
+  addProduct(product: Product): void {
+    const existingProductIndex = this.productList.findIndex(p => p.id === product.id);
+    if (existingProductIndex !== -1) {
+      // @ts-ignore
+      this.productList[existingProductIndex].quantitySelected++;
+      // @ts-ignore
+      this.productList[existingProductIndex].priceTotalSale = this.productList[existingProductIndex].priceSale * this.productList[existingProductIndex].quantitySelected;
+
+    } else {
+      this.productList.push({...product, quantitySelected: 1});
+    }
     this.saveProducts();
   }
+
 
   removeProduct(product: Product) {
     const index = this.productList.indexOf(product);
     if (index > -1) {
       this.productList.splice(index, 1);
-      this.saveProducts();
     }
+    this.updateProductSalePrice();
   }
 
   saveProducts() {
     localStorage.setItem('productList', JSON.stringify(this.productList));
+  }
+
+  updateQuantity(quantity: number, product: Product): void {
+
+    const index = this.productList.findIndex(p => p.id === product.id);
+    // @ts-ignore
+    this.productList[index].quantitySelected = quantity;
+    // @ts-ignore
+    this.productList[index].priceTotalSale = this.productList[index].priceSale * this.productList[index].quantitySelected;
+
+    this.updateProductSalePrice();
+    this.saveProducts();
   }
 
   loadProducts() {
@@ -45,4 +73,24 @@ export class CartComponent implements OnInit {
       this.productList = JSON.parse(storedProducts);
     }
   }
+
+  updateProductSalePrice() {
+    this.productList.forEach(p => {
+      if (p.quantitySelected === 1) {
+        p.priceTotalSale = p.priceSale;
+      }
+    });
+    this.updateSalePrice()
+    this.saveProducts();
+  }
+
+  updateSalePrice() {
+    this.salePrice = 0
+    this.productList.forEach(p => {
+      if (p.priceTotalSale != undefined) {
+        this.salePrice += p.priceTotalSale;
+      }
+    })
+  }
 }
+
