2ad2dfe4a8e7cf8342a91a9b1f0391f3b7485e12
Geraldo Daros <78937716+geraldodf@users.noreply.github.com>
2023-02-21 19:46:43 +0100

    Adding template for adress and payment.

--- src/app/templates/cart/cart.component.ts
+++ src/app/templates/cart/cart.component.ts
@@ -1,43 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {ProductServiceService} from "../product-service.service";
-import {Product} from "../../../models/Product";
+import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../product-service.service';
+import { Product } from '../../../models/Product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
-  constructor(private productService: ProductServiceService) {
-  }
+  constructor(private productService: ProductServiceService) {}
 
   productList: Product[] = [];
   list: Product[] = [];
   salePrice: number = 0;
+  showPayments = false;
+
 
   ngOnInit() {
     this.loadProducts();
     this.initCart();
     this.updateProductSalePrice();
-    this.updateSalePrice()
+    this.updateSalePrice();
   }
 
   initCart() {
-    this.productService.getProductsCart().forEach(p => {
-      this.addProduct(p)
-    })
+    this.productService.getProductsCart().forEach((p) => {
+      this.addProduct(p);
+    });
   }
 
   addProduct(product: Product): void {
-    const existingProductIndex = this.productList.findIndex(p => p.id === product.id);
+    const existingProductIndex = this.productList.findIndex(
+      (p) => p.id === product.id
+    );
     if (existingProductIndex !== -1) {
-
       this.productList[existingProductIndex].quantitySelected++;
-      this.productList[existingProductIndex].priceTotalSale = this.productList[existingProductIndex].priceSale * this.productList[existingProductIndex].quantitySelected;
-
+      this.productList[existingProductIndex].priceTotalSale =
+        this.productList[existingProductIndex].priceSale *
+        this.productList[existingProductIndex].quantitySelected;
     } else {
-      this.productList.push({...product, quantitySelected: 1});
+      this.productList.push({ ...product, quantitySelected: 1 });
     }
     this.saveProducts();
   }
@@ -61,10 +63,11 @@ export class CartComponent implements OnInit {
   }
 
   updateQuantity(quantity: number, product: Product): void {
-
-    const index = this.productList.findIndex(p => p.id === product.id);
+    const index = this.productList.findIndex((p) => p.id === product.id);
     this.productList[index].quantitySelected = quantity;
-    this.productList[index].priceTotalSale = this.productList[index].priceSale * this.productList[index].quantitySelected;
+    this.productList[index].priceTotalSale =
+      this.productList[index].priceSale *
+      this.productList[index].quantitySelected;
 
     this.updateProductSalePrice();
     this.saveProducts();
@@ -78,22 +81,25 @@ export class CartComponent implements OnInit {
   }
 
   updateProductSalePrice() {
-    this.productList.forEach(p => {
+    this.productList.forEach((p) => {
       if (p.quantitySelected === 1) {
         p.priceTotalSale = p.priceSale;
       }
     });
-    this.updateSalePrice()
+    this.updateSalePrice();
     this.saveProducts();
   }
 
   updateSalePrice() {
-    this.salePrice = 0
-    this.productList.forEach(p => {
+    this.salePrice = 0;
+    this.productList.forEach((p) => {
       if (p.priceTotalSale != undefined) {
         this.salePrice += p.priceTotalSale;
       }
-    })
+    });
+  }
+
+  paymentMethod() {
+    console.log('Finalizei a venda!');
   }
 }
-
